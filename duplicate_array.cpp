6c335fad4d2688e0d92b3fa316d1094094079297
AjayBadhe <ajaybadhe2024@gmail.com>
2025-11-26 19:33:17 +0530

    duplicate_array

---
+++ duplicate_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+     
+    // Write your code here
+    int arr[5]={1,2,3,4,4};
+    int ans=0;
+    for(int i=0;i<5;i++){
+        ans=ans^arr[i];
+    }
+    for(int i=1;i<5;i++){
+        ans=ans^i;
+    }
+	cout<<ans;
+
+
+return 0;
+} 
